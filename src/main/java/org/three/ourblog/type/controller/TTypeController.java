6ed61182cac54fd96939cd0292bfcab9257a12d6
shuiZe <1773085230@qq.com>
2020-06-23 15:01:59 +0800

    添加获取博客类型接口

--- src/main/java/org/three/ourblog/type/controller/TTypeController.java
+++ src/main/java/org/three/ourblog/type/controller/TTypeController.java
@@ -1,9 +1,16 @@
 package org.three.ourblog.type.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.three.ourblog.type.pojo.TType;
+import org.three.ourblog.type.service.TTypeService;
+import org.three.ourblog.uitls.RespUtil;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-06-18
  */
 @RestController
-@RequestMapping("/type/t-type")
+@RequestMapping("/type")
 public class TTypeController {
+    @Autowired
+    private TTypeService typeService;
 
+    @GetMapping
+    public RespUtil getAllBlogType(){
+        List<TType> types = typeService.list(null);
+        return RespUtil.ok().data("data", types);
+    }
 }
 
