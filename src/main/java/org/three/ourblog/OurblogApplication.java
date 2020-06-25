bd5f7a8efe636a6b4a7de0463119e019f9614f50
shuiZe <1773085230@qq.com>
2020-06-25 16:26:49 +0800

    配置mysql性能分析插件

--- src/main/java/org/three/ourblog/OurblogApplication.java
+++ src/main/java/org/three/ourblog/OurblogApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OurblogApplication {
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(OurblogApplication.class, args);
     }
 
--- src/main/java/org/three/ourblog/config/MybatisConfig.java
+++ src/main/java/org/three/ourblog/config/MybatisConfig.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,4 +28,6 @@ public class MybatisConfig {
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         return paginationInterceptor;
     }
+
+
 }
